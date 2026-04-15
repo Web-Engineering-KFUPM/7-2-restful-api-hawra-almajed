ea94769ab33d452eaac413bb9548b4052215a375
hawraalmajed <s202333090@kfupm.edu.sa>
2026-04-15 20:55:54 +0300

    todo2

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
+// db schema
 const songSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  artist: { type: String, required: true, trim: true },
-  year: { type: Number, min: 1900, max: new Date().getFullYear() }
-}, { timestamps: true });
+        title:  { type: String, required: required, trim: true },
+        artist: { type: String, required: required, trim: true },
+        year:   { type: Numer, min: 1900, max: new Date() }
+      }, { timestamps: true });
+
 
 export const Song = mongoose.model("Song", songSchema);
